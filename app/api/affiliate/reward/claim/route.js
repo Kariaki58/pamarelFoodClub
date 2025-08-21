1096bf433403480643985df3524c701e2e0cc16a
Kariaki58 <kariakistephen809@gmail.com>
2025-08-21 19:29:50 +0100

    pusing code

--- app/api/affiliate/reward/claim/route.js
+++ app/api/affiliate/reward/claim/route.js
@@ -1,7 +1,8 @@
 import { NextResponse } from "next/server";
 import connectToDatabase from "@/lib/dbConnect";
 import User from "@/models/user";
-import PLANS from "../../../../../lib/plans";
+
+import { PLANS } from "../../../../../lib/plans";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/options";
 
