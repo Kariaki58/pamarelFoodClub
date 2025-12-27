d03c3af55ffe7477a4ebf6fc79780e27d98aed2f
kariaki58 <kariakistephen809@gmail.com>
2025-12-27 19:50:38 +0000

    added whatsapp number

--- components/landing-page/PamarelLandingPage.jsx
+++ components/landing-page/PamarelLandingPage.jsx
@@ -137,7 +137,7 @@ const PamarelLandingPage = () => {
       </div>
       {/* Floating WhatsApp Button */}
       <a
-        href="https://wa.me/+2348106042246"
+        href="https://wa.me/+2349040498445"
         target="_blank"
         rel="noopener noreferrer"
         className="fixed bottom-6 right-6 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition duration-300 z-50 flex items-center justify-center"
